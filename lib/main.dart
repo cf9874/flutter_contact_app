6e985de22e6f30737f723b9cdf3e0bd1d4e874cf
cf9874 <cf9874@gmail.com>
2025-10-28 14:08:43 +0900

    basic widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Center(
+        // child: Text('asdsa')
+        //   child: Icon(Icons.safety_check)
+        // child: Image.asset('alp.jpg')
+        child: Container(width: 50,height: 50,color: Colors.cyan)
+      ),
+    );
   }
 }
-
+// widget
+// 1. Text
+// 2. Image << add seets dir  , pubspec.yaml and set path
+// 3. Icon
+// 4. Box
